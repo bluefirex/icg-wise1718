d5d2bc23288536cd5b5c4caebff422700bc1a1de
bluefirex <github@bfx.re>
2017-12-01 14:45:38 +0100

    Add 3D Cube OOP

---
+++ 3D Cube object oriented/cube.js
@@ -0,0 +1,263 @@
+// Environment variables
+let gl,
+	canvas;
+
+// Scene variables
+let objects = [];
+
+// Shader variables
+let program;
+
+let pointLoc,
+	colorLoc;
+
+let modelMatrixLoc;
+
+let viewMatrixLoc,
+	viewMatrix;
+
+let projectionMatrixLoc,
+	projectionMatrix;
+
+function degToRad (deg) {
+	return deg * Math.PI / 180;
+}
+
+class Cube {
+	constructor (from = {x: -0.5, y: -0.5, z: -0.5}, to = {x: 0.5, y: 0.5, z: 0.5}, sideColors = {front: [0, 0, 1, 1], right: [0, 1, 0, 1], back: [1, 0, 0, 1], left: [1, 1, 0, 1], bottom: [1, 0, 1, 1], top: [0, 1, 1, 1]}) {
+		this.from = from;
+		this.to = to;
+		this.sideColors = sideColors;
+		this.mesh = [];
+		this.colors = [];
+		this.orientation = {x: 0, y: 0, z: 0};
+		this.position = {x: 0, y: 0, z: 0};
+		this.verticesVBO = gl.createBuffer();
+		this.modelMatrix = this.SetModelMatrix(this.position, this.orientation);
+
+		this.MakeModel();
+		this.InitBuffer();
+	}
+
+	/**
+	 * Makes the model, namely the mesh and the colors arrays
+	 */
+	MakeModel () {
+		this.mesh = [
+			// Front
+			this.from.x, this.from.y, this.to.z,
+			this.to.x, this.from.y, this.to.z,
+			this.from.x, this.to.y, this.to.z,
+
+			this.to.x, this.to.y, this.to.z,
+			this.from.x, this.to.y, this.to.z,
+			this.to.x, this.from.y, this.to.z,
+
+			// Right
+			this.to.x, this.to.y, this.to.z,
+			this.to.x, this.from.y, this.to.z,
+			this.to.x, this.from.y, this.from.z,
+
+			this.to.x, this.to.y, this.from.z,
+			this.to.x, this.to.y, this.to.z,
+			this.to.x, this.from.y, this.from.z,
+
+			// Back
+			this.from.x, this.from.y, this.from.z,
+			this.to.x, this.from.y, this.from.z,
+			this.from.x, this.to.y, this.from.z,
+
+			this.to.x, this.to.y, this.from.z,
+			this.from.x, this.to.y, this.from.z,
+			this.to.x, this.from.y, this.from.z,
+
+			// Left
+			this.from.x, this.to.y, this.to.z,
+			this.from.x, this.from.y, this.to.z,
+			this.from.x, this.from.y, this.from.z,
+
+			this.from.x, this.to.y, this.from.z,
+			this.from.x, this.to.y, this.to.z,
+			this.from.x, this.from.y, this.from.z,
+
+			// Bottom
+			this.from.x, this.from.y, this.to.z,
+			this.from.x, this.from.y, this.from.z,
+			this.to.x, this.from.y, this.to.z,
+
+			this.to.x, this.from.y, this.from.z,
+			this.from.x, this.from.y, this.from.z,
+			this.to.x, this.from.y, this.to.z,
+
+			// Top
+			this.from.x, this.to.y, this.to.z,
+			this.from.x, this.to.y, this.from.z,
+			this.to.x, this.to.y, this.to.z,
+
+			this.to.x, this.to.y, this.from.z,
+			this.from.x, this.to.y, this.from.z,
+			this.to.x, this.to.y, this.to.z
+		]
+
+		for (let i = 0; Math.floor(i/6) < 6; i++) {
+
+			this.colors = this.colors.concat(Object.values(this.sideColors)[Math.floor(i/6)]);
+
+		}
+	}
+
+	/**
+	 * Sets the model matrix
+	 * @param {Object} position x,y,z
+	 * @param {Object} orientation x,y,z - angles in degree
+	 */
+	SetModelMatrix (position, orientation) {
+		
+		// Convert the orientation to RAD
+		orientation = {x: degToRad(orientation.x), y: degToRad(orientation.y), z: degToRad(orientation.z)};
+	
+		// Set the transformation matrix
+		return [
+			1, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 0, 1, 0,
+			position.x, position.y, position.z, 1
+		];
+	}
+
+	/**
+	 * Sets the buffer data
+	 */
+	InitBuffer () {
+		gl.useProgram(program);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.verticesVBO);
+
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.mesh.concat(this.colors)), gl.STATIC_DRAW);
+	}
+
+	/**
+	 * Updates the model matrix to the buffer
+	 */
+	UpdateBuffer () {
+		// Push the matrix to the buffer
+		gl.uniformMatrix4fv(modelMatrixLoc, false, new Float32Array(this.modelMatrix));		
+	}
+
+	Render () {
+		
+		// Bind the program and the vertex buffer object
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.verticesVBO);
+
+		// Set attribute pointers and enable them
+		gl.vertexAttribPointer(pointLoc, 3, gl.FLOAT, false, 0, 0);
+		gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, this.mesh.length*4);
+		gl.enableVertexAttribArray(pointLoc);
+		gl.enableVertexAttribArray(colorLoc);
+
+		// Set uniforms
+		this.UpdateBuffer();
+
+		// Draw the object
+		gl.drawArrays(gl.TRIANGLES, 0, this.mesh.length/3);
+	}
+}
+
+/**
+ * Initializes the program, models and shaders
+ */
+function init() {
+
+	// 1. Get canvas and setup WebGL context
+    canvas = document.getElementById("gl-canvas");
+	gl = canvas.getContext('webgl');
+	
+	// 2. Configure viewport
+	gl.viewport(0,0,canvas.width,canvas.height);
+	gl.clearColor(0.95,0.95,0.95,1.0);
+	gl.enable(gl.DEPTH_TEST);
+
+	// 3. Specify vertices
+	objects.push(new Cube(
+		{
+			x: 0.2,
+			y: 0,
+			z: 0
+		},
+
+		{
+			x: 0.4,
+			y: 0.2,
+			z: 0.2
+		}
+	));
+
+	objects.push(new Cube(
+		{
+			x: -0.2,
+			y: 0,
+			z: 0	
+		},
+
+		{
+			x: 0,
+			y: 0.2,
+			z: 0.2
+		},
+
+		{
+			front: [0.4, 0.4, 0.4, 1],
+			back: [0.2, 0.2, 0.2, 1],
+			left: [0.6, 0.6, 0.6, 1],
+			right: [0.8, 0.8, 0.8, 1],
+			top: [0.5, 0.5, 0.5, 1],
+			bottom: [0.5, 0.5, 0.5, 1],
+		}
+	));
+
+	// 4. Init shader program via additional function and bind it
+	program = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(program);
+
+	// 7 Save attribute location to address them
+	pointLoc = gl.getAttribLocation(program, "vPosition");
+	colorLoc = gl.getAttribLocation(program, "vColor");
+	modelMatrixLoc = gl.getUniformLocation(program, "modelMatrix");
+
+    // Set view matrix
+	let eyeVec = vec3.fromValues(0.4, 0.4, 1.0);
+	let target = vec3.fromValues(0.0, 0.0, 0.0);
+	let up = vec3.fromValues(0.0, 1.0, 0.0);
+
+	viewMatrix = mat4.create();
+	mat4.lookAt(viewMatrix, eyeVec, target, up);
+
+	// 7 Save uniform location and save the view matrix into it
+	viewMatrixLoc = gl.getUniformLocation(program, "viewMatrix");
+	gl.uniformMatrix4fv(viewMatrixLoc, false, viewMatrix);
+
+    // Set projection matrix
+
+	projectionMatrix = mat4.create();
+	mat4.perspective(projectionMatrix, Math.PI * 0.25, canvas.width / canvas.height, 0.5, 100);
+
+	// 7 Save uniform location and save the projection matrix into it
+	projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+	gl.uniformMatrix4fv(projectionMatrixLoc, false, projectionMatrix);
+	
+	// 8. Render
+	render();
+};
+
+function render()
+{
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	// Call every render function
+    objects.forEach(function(object) {
+		object.Render();
+	});
+
+	requestAnimationFrame(render);
+}
+
+init ();
