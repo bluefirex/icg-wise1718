53cba19ac0d9225cba00e90c010688103f485677
bluefirex <github@bfx.re>
2018-01-07 17:45:45 +0100

    Show debug info when shader compilation fails

--- 3D Insel/common/initShaders.js
+++ 3D Insel/common/initShaders.js
@@ -2,45 +2,55 @@
 //  initShaders.js
 //
 
-function initShaders( gl, vertexShaderId, fragmentShaderId )
-{
-    const compileShader = ( gl, gl_shaderType, shaderSource ) => {
-        // create vertex shader
-        shader = gl.createShader( gl_shaderType );
+function initShaders(gl, vertexShaderId, fragmentShaderId) {
+	const compileShader = (gl, gl_shaderType, shaderSource) => {
+		// create vertex shader
+		shader = gl.createShader(gl_shaderType)
 
-        // set the shader source code
-        gl.shaderSource( shader, shaderSource );
+		// set the shader source code
+		gl.shaderSource(shader, shaderSource)
 
-        // compile the shader to make it readable for the GPU
-        gl.compileShader( shader );
-        var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-        
-        if (!success) {
-            // Something went wrong during compilation; get the error
-            throw "could not compile shader:" + gl.getShaderInfoLog(shader);
-        }
-        else {
-            return shader;
-        }
-    }
-    
-    /** 
-     * Setup shader program
-     */
-    vShaderSource = document.querySelector( '#' + vertexShaderId ).text;
-    fShaderSource = document.querySelector( '#' + fragmentShaderId ).text;
+		// compile the shader to make it readable for the GPU
+		gl.compileShader(shader)
 
-    vertexShader = compileShader( gl, gl.VERTEX_SHADER, vShaderSource );
-    fragmentShader = compileShader( gl, gl.FRAGMENT_SHADER, fShaderSource );
+		const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+		
+		if (!success) {
+			// Something went wrong during compilation; get the error
+			const shaderError = gl.getShaderInfoLog(shader)
 
-    // Build the program
-    const program = gl.createProgram();
+			console.warn('Could not compile shader #' + vertexShaderId)
 
-    // Attach shaders to it
-    gl.attachShader( program, vertexShader );
-    gl.attachShader( program, fragmentShader );
+			for (let error of shaderError.split("\n").filter(x => x.length > 1)) {
+				const [dontcare, errorColumn, errorLine] = error.match(/ERROR: ([0-9]+):([0-9]+)/)
+				const line = shaderSource.split("\n")[errorLine - 1]
 
-    gl.linkProgram( program );
+				console.warn('Error in Line ' + errorLine + ': ', line)
+			}
 
-    return program;
+			throw gl.getShaderInfoLog(shader)
+		} else {
+			return shader
+		}
+	}
+	
+	/** 
+	 * Setup shader program
+	 */
+	vShaderSource = document.querySelector('#' + vertexShaderId).text
+	fShaderSource = document.querySelector('#' + fragmentShaderId).text
+
+	vertexShader = compileShader(gl, gl.VERTEX_SHADER, vShaderSource)
+	fragmentShader = compileShader(gl, gl.FRAGMENT_SHADER, fShaderSource)
+
+	// Build the program
+	const program = gl.createProgram()
+
+	// Attach shaders to it
+	gl.attachShader(program, vertexShader)
+	gl.attachShader(program, fragmentShader)
+
+	gl.linkProgram(program)
+
+	return program
 }
